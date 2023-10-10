0c1bf7dd72ac9a5eeeec63c37ec49f7b80f3165e
LucasMoore123 <luamoore@ucsc.edu>
2023-10-10 13:12:03 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Lucas Game!!!";
+const gameName = "Lucas' Kitchen";
+const buttonName = "🍲";
 
 document.title = gameName;
 
+// header creation
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// button creation
+const button = document.createElement("button");
+button.textContent = buttonName;
+app.append(button);

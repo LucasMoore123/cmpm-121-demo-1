40ef10c32ff19289d4b20bc2105cffa35db3acbf
LucasMoore123 <luamoore@ucsc.edu>
2023-10-10 13:46:23 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,21 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// counter creation
+let counter: number = 0;
+const count = document.createElement("div");
+count.style.fontSize = "2em";
+count.innerHTML = counter + " Meals Created!";
+app.append(count);
+
 // button creation
 const button = document.createElement("button");
 button.textContent = buttonName;
 app.append(button);
+
+// counter gain logic/updating
+const gain: number = 1;
+button.addEventListener("click", () => {
+    counter = counter + gain;
+    count.innerHTML = counter + " Meals Created!";
+});

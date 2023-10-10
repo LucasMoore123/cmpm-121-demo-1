0b999e05176cdd71ade4b7d5ea06d28d59d5014f
LucasMoore123 <luamoore@ucsc.edu>
2023-10-10 14:27:53 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,10 @@ header.innerHTML = gameName;
 app.append(header);
 
 // counter creation
-let counter: number = 0;
+let total_meals: number = 0;
 const count = document.createElement("div");
 count.style.fontSize = "2em";
-count.innerHTML = counter + " Meals Created!";
+count.innerHTML = total_meals + " Meals Created!";
 app.append(count);
 
 // button creation
@@ -25,16 +25,29 @@ button.textContent = buttonName;
 app.append(button);
 
 // counter gain logic/updating
-const gain: number = 1;
+const gain_per_click: number = 1;
 button.addEventListener("click", () => {
-    updateCounter();
+  updateCounter();
 });
 
 // counter updating function
-function updateCounter(){
-    counter += gain;
-    count.innerHTML = counter + " Meals Created!";
+function updateCounter() {
+  total_meals += gain_per_click;
+  count.innerHTML = total_meals + " Meals Created!";
 }
 
-// counter automatic +1
-setInterval(updateCounter, 1000);
+// automatic counter
+function automaticCounter(){
+  const increasePerSecond = 1; // Number to increase
+  let lastTimestamp = performance.now() / 1000; // Initial timestamp
+  function increaseCounter(){
+    const currentTimestamp = performance.now() / 1000; // Latest timestamp
+    const deltaTime = currentTimestamp - lastTimestamp; // Difference
+    total_meals += increasePerSecond * deltaTime; // Fraction to increase by
+    count.innerHTML = total_meals.toFixed(3) + " Meals Created!";
+    lastTimestamp = currentTimestamp;
+    requestAnimationFrame(increaseCounter);
+  }
+  requestAnimationFrame(increaseCounter);
+}
+automaticCounter();

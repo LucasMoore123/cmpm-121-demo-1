4ffb2b35891fb1f06a59cb85f846688e61d340a2
LucasMoore123 <luamoore@ucsc.edu>
2023-10-12 14:17:01 -0700

    Step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -3,39 +3,115 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Lucas' Kitchen";
-const buttonName = "🍲";
-const upgrade_one = "Customer👨🏽(+1 meal per second)";
+const buttonName = "🍲Click to cook!";
+const buttons: HTMLButtonElement[] = [];
 let increasePerSecond: number = 0; // Number to increase
+const buttonItemMap = new Map<HTMLButtonElement, Item>(); // SOURCE: https://howtodoinjava.com/typescript/maps/
 
 document.title = gameName;
 
-// header creation
+// Creating new item interface
+interface Item {
+  name: string;
+  cost: number;
+  units_bought: number;
+  increase_per: number;
+  description: string;
+}
+
+// Creating new item array
+const availableItems: Item[] = [
+  {
+    name: "Customer👨🏽",
+    cost: 10,
+    units_bought: 0,
+    increase_per: 0.1,
+    description: "Fueling the economy.",
+  },
+  {
+    name: "Chef👨🏿‍🍳",
+    cost: 100,
+    units_bought: 0,
+    increase_per: 2,
+    description: "A culinary master.",
+  },
+  {
+    name: "Manager🕴🏻",
+    cost: 1000,
+    units_bought: 0,
+    increase_per: 50,
+    description: "Does he actually do any work?",
+  },
+];
+
+// GAME TITLE CREATION
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// counter creation
+// MEALS CREATED COUNTER CREATION
 let total_meals: number = 0;
 const count = document.createElement("div");
 count.style.fontSize = "2em";
 count.innerHTML = total_meals + " Meals Created!";
 app.append(count);
 
+// MEALS PER SECOND COUNTER CREATION
+const mps_count = document.createElement("div");
+mps_count.style.fontSize = "1em";
+mps_count.innerHTML = increasePerSecond.toFixed(2) + " meals per second";
+app.append(mps_count);
+
 // gain button creation
 const button = document.createElement("button");
 button.textContent = buttonName;
 app.append(button);
 
-// upgrade button creation
-const upgrade_button_one = document.createElement("button");
-upgrade_button_one.textContent = upgrade_one;
-app.append(upgrade_button_one);
+// new spacer division
+const spacer = document.createElement("div");
+spacer.style.clear = "both";
+app.append(spacer);
 
-// upgrade button onClick event
-upgrade_button_one.disabled = true;
-upgrade_button_one.addEventListener("click", () => {
-  total_meals -= 10;
-  increasePerSecond += 1;
+// used on items to create upgrade buttons
+function createUpgradeButton(item: Item): void {
+  const newButton = document.createElement("button");
+  newButton.innerHTML =
+    item.name +
+    "<br>" +
+    item.description +
+    "<br><span style='font-size: 0.5em;'>cost: " +
+    item.cost.toFixed(2) +
+    " | Owned: " +
+    item.units_bought +
+    " | mps: " +
+    item.increase_per +
+    "</span>";
+  buttonItemMap.set(newButton, item);
+  newButton.addEventListener("click", () => {
+    total_meals -= item.cost;
+    increasePerSecond += item.increase_per;
+    item.cost *= 1.15;
+    item.units_bought += 1;
+    newButton.innerHTML =
+      item.name +
+      "<br>" +
+      item.description +
+      "<br><span style='font-size: 0.5em;'>cost: " +
+      item.cost.toFixed(2) +
+      " | Owned: " +
+      item.units_bought +
+      " | mps: " +
+      item.increase_per +
+      "</span>";
+    updateCounter();
+  });
+  app.appendChild(newButton);
+  buttons.push(newButton);
+}
+
+// create upgrade buttons
+availableItems.forEach((item) => {
+  createUpgradeButton(item);
 });
 
 // counter gain logic/updating
@@ -49,20 +125,31 @@ function updateCounter() {
   total_meals += gain_per_click;
   count.innerHTML = total_meals + " Meals Created!";
 }
-
+// button update checking function
+function updateButtonStates() {
+  buttons.forEach((button) => {
+    const buttonItem = buttonItemMap.get(button);
+    if (buttonItem) {
+      if (buttonItem.cost > total_meals) {
+        button.disabled = true;
+      } else {
+        button.disabled = false;
+      }
+    }
+  });
+}
 // automatic counter
 function automaticCounter() {
   let lastTimestamp = performance.now() / 1000; // Initial timestamp
   function increaseCounter() {
-    if (total_meals < 10) {
-      upgrade_button_one.disabled = true;
-    } else {
-      upgrade_button_one.disabled = false;
-    }
+    // hijacking counter to do button enable/disable
+    updateButtonStates();
+    // and mps counter
+    mps_count.innerHTML = increasePerSecond.toFixed(2) + " meals per second";
     const currentTimestamp = performance.now() / 1000; // Latest timestamp
     const deltaTime = currentTimestamp - lastTimestamp; // Difference
     total_meals += increasePerSecond * deltaTime; // Fraction to increase by
-    count.innerHTML = total_meals.toFixed(3) + " Meals Created!";
+    count.innerHTML = total_meals.toFixed(2) + " Meals Created!";
     lastTimestamp = currentTimestamp;
     requestAnimationFrame(increaseCounter);
   }
